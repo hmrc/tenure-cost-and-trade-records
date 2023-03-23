7b9685c638f2593c7d04efa47e18ab26c4fab5ce
peterdrobinson <77154273+peterdrobinson@users.noreply.github.com>
2023-03-23 13:47:08 +0000

    BST-54809 - WIP for backend connection for NotConnected

---
+++ app/uk/gov/hmrc/tctr/backend/controllers/NotConnectedSubmissionController.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tctr.backend.controllers
+
+import play.api.Logger
+import play.api.mvc.ControllerComponents
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.tctr.backend.metrics.MetricsHandler
+import uk.gov.hmrc.tctr.backend.models.{NotConnectedSubmission, NotConnectedSubmissionForm}
+import uk.gov.hmrc.tctr.backend.repository.{NotConnectedRepository, SubmittedMongoRepo}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class NotConnectedSubmissionController @Inject()(repository: NotConnectedRepository,
+                                                 submittedMongoRepo: SubmittedMongoRepo,
+                                                 metric: MetricsHandler, cc: ControllerComponents)(implicit ec: ExecutionContext) extends BackendController(cc) {
+
+  val log = Logger(classOf[NotConnectedSubmissionController])
+
+  def submit(submissionReference: String) = Action.async(parse.json[NotConnectedSubmissionForm]) { request =>
+
+    submittedMongoRepo.hasBeenSubmitted(submissionReference) flatMap {
+      case true => {
+        metric.failedSubmissions.mark()
+        log.warn(s"Error saving submission $submissionReference. Possible duplicate")
+        Conflict(s"Error saving submission $submissionReference. Possible duplicate")
+      }
+      case false => {
+        repository.insert(convertFormToEntity(request.body))
+        metric.okSubmissions.mark()
+        Created
+      }
+    }
+  }
+
+  private def convertFormToEntity(form: NotConnectedSubmissionForm) = NotConnectedSubmission(
+    form.id, form.address, form.fullName, form.emailAddress,
+    form.phoneNumber, form.additionalInformation, form.createdAt, form.previouslyConnected, form.lang)
+
+}
---
+++ app/uk/gov/hmrc/tctr/backend/models/NotConnectedSubmission.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tctr.backend.models
+
+import play.api.libs.json._
+import uk.gov.hmrc.mongo.play.json.formats.MongoFormats.mongoEntity
+import uk.gov.hmrc.tctr.backend.schema.Address
+
+import java.time.Instant
+import scala.annotation.nowarn
+
+case class NotConnectedSubmission(id: String,
+                                  address: Address,
+                                  fullName: String,
+                                  emailAddress: Option[String],
+                                  phoneNumber: Option[String],
+                                  additionalInformation: Option[String],
+                                  createdAt: Instant,
+                                  previouslyConnected: Option[Boolean],
+                                  lang: Option[String] = None
+                                 )
+
+object NotConnectedSubmission {
+  import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats.Implicits._
+
+  @nowarn
+  implicit val format: Format[NotConnectedSubmission] = mongoEntity {
+    Json.format[NotConnectedSubmission]
+  }
+
+}
