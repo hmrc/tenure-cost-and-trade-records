e67a43b3bdeff48d91580786e7537bb5f4a13c71
Yuriy Tumakha <tumakha@gmail.com>
2023-03-24 01:09:52 +0000

    BST-46989 Unit test `EncryptionJsonTransformerSpec`, added scalatest `BeEncryptedMatcher`

---
+++ test/uk/gov/hmrc/tctr/backend/crypto/BeEncryptedMatchers.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tctr.backend.crypto
+
+import org.scalatest.matchers.{BeMatcher, MatchResult}
+import play.api.libs.json.JsLookupResult
+
+/**
+  * @author Yuriy Tumakha
+  */
+trait BeEncryptedMatchers {
+
+  class BeEncryptedMatcher extends BeMatcher[JsLookupResult] {
+
+    private val base64RegExp =
+      """^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$"""
+
+    def apply(jsLookupResult: JsLookupResult): MatchResult = {
+      val value = jsLookupResult.toOption.fold("")(_.as[String])
+      MatchResult(
+        checkValueIsEncrypted(value),
+        s"Value `$value` wasn't encrypted",
+        s"Value `$value` was encrypted"
+      )
+    }
+
+    private def checkValueIsEncrypted(str: String): Boolean =
+      str.length > 50 && str.matches(base64RegExp)
+
+  }
+
+  val encrypted = new BeEncryptedMatcher
+}
+
+object BeEncryptedMatchers extends BeEncryptedMatchers
---
+++ test/uk/gov/hmrc/tctr/backend/crypto/EncryptionJsonTransformerSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tctr.backend.crypto
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.libs.json._
+
+import play.api.test.Injecting
+
+import scala.io.Source
+
+/**
+  * @author Yuriy Tumakha
+  */
+class EncryptionJsonTransformerSpec
+    extends AnyFlatSpec
+    with should.Matchers
+    with Injecting
+    with GuiceOneAppPerSuite
+    with BeEncryptedMatchers {
+
+  private val encryptionJsonTransformer = inject[EncryptionJsonTransformer]
+  private val submissionDraftJson       = Json.parse(Source.fromResource("json/submissionDraft.json").mkString)
+
+  "EncryptionJsonTransformer" should "return original submissionDraftJson after encrypt then decrypt" in {
+    val encryptedJson = encryptionJsonTransformer.encrypt(submissionDraftJson)
+    val decryptedJson = encryptionJsonTransformer.decrypt(encryptedJson)
+    decryptedJson shouldBe submissionDraftJson
+  }
+
+  it                          should "encrypt sensitive PII fields" in {
+    val encryptedJson = encryptionJsonTransformer.encrypt(submissionDraftJson)
+
+    (encryptedJson \ "session" \ "saveAsDraftPassword")                                          shouldBe encrypted
+    (encryptedJson \ "session" \ "userLoginDetails" \ "token")                                   shouldBe encrypted
+    (encryptedJson \ "session" \ "userLoginDetails" \ "address" \ "buildingNameNumber")          shouldBe encrypted
+    (encryptedJson \ "session" \ "userLoginDetails" \ "address" \ "street1")                     shouldBe encrypted
+    (encryptedJson \ "session" \ "userLoginDetails" \ "address" \ "street2")                     shouldBe encrypted
+    (encryptedJson \ "session" \ "userLoginDetails" \ "address" \ "postcode")                    shouldBe encrypted
+    (encryptedJson \ "session" \ "aboutYou" \ "customerDetails" \ "fullName")                    shouldBe encrypted
+    (encryptedJson \ "session" \ "aboutYou" \ "customerDetails" \ "contactDetails" \ "phone")    shouldBe encrypted
+    (encryptedJson \ "session" \ "aboutYou" \ "customerDetails" \ "contactDetails" \ "email")    shouldBe encrypted
+    (encryptedJson \ "session" \ "other" \ "sensitivePII" \ "previousAddress")                   shouldBe encrypted
+    (encryptedJson \ "session" \ "other" \ "sensitivePII" \ "newAddress" \ "buildingNameNumber") shouldBe encrypted
+    (encryptedJson \ "session" \ "other" \ "sensitivePII" \ "newAddress" \ "street1")            shouldBe encrypted
+    (encryptedJson \ "session" \ "other" \ "sensitivePII" \ "newAddress" \ "town")               shouldBe encrypted
+    (encryptedJson \ "session" \ "other" \ "sensitivePII" \ "newAddress" \ "postcode")           shouldBe encrypted
+  }
+
+  it                          should "not encrypt not PII fields" in {
+    val encryptedJson = encryptionJsonTransformer.encrypt(submissionDraftJson)
+    println("Encrypted JSON: " + Json.prettyPrint(encryptedJson))
+
+    (encryptedJson \ "exitPath") shouldBe (submissionDraftJson \ "exitPath")
+
+    (encryptedJson \ "session" \ "other" \ "sensitivePII" \ "newAddress" \ "county") shouldBe
+      (submissionDraftJson \ "session" \ "other" \ "sensitivePII" \ "newAddress" \ "county")
+  }
+
+}
