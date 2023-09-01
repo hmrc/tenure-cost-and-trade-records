8b3d9521b1341816508087640df72f8fcccd4cfa
Yuriy Tumakha <tumakha@gmail.com>
2023-09-01 10:21:03 +0100

    BST-66482 !!! Temporary database MUST be deleted after each test

--- test/uk/gov/hmrc/tctr/backend/repository/MongoSpecBase.scala
+++ test/uk/gov/hmrc/tctr/backend/repository/MongoSpecBase.scala
@@ -46,9 +46,10 @@ abstract class MongoSpecBase
 
   implicit val ec: ExecutionContext = inject[ExecutionContext]
 
-  override protected def afterAll(): Unit =
-//    mongo.database.drop().toFutureOption().futureValue
+  override protected def afterAll(): Unit = {
+    mongo.database.drop().toFutureOption().futureValue // !!! Temporary database MUST be deleted after each test
     mongo.client.close()
+  }
 
   override final def fakeApplication(): Application = new GuiceApplicationBuilder()
     .configure(
