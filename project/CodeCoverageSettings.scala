e0971c5c82b3147b1313b0509bcb14efeaf5ecc2
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2024-01-25 11:06:31 +0000

    BST-66870 Increase the coverage to 61%

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -16,7 +16,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 59,
+    ScoverageKeys.coverageMinimumStmtTotal := 61,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
