68fafeb86587e58830b337fa91579fd91f5c6873
Yuriy Tumakha <tumakha@gmail.com>
2024-03-25 08:16:14 +0000

    BST-86189 New TradingHistory property `turnoverSections6020: Option[Seq[TurnoverSection6020]]` and new model `TurnoverSection6020` (#92)
    * BST-86189 New TradingHistory property `turnoverSections6020: Option[Seq[TurnoverSection6020]]` and new model `TurnoverSection6020`
    
    * BST-86189 New TradingHistory property `turnoverSections6020: Option[Seq[TurnoverSection6020]]` and new model `TurnoverSection6020`

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -18,7 +18,7 @@ object CodeCoverageSettings extends AutoPlugin {
 
   override val projectSettings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 63,
+    ScoverageKeys.coverageMinimumStmtTotal := 64,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
