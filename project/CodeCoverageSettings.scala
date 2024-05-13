dc7da724dcc454d2901bb48e499dd82426c8ba65
Yuriy Tumakha <tumakha@gmail.com>
2024-05-13 13:51:10 +0100

    BST-91959 Minimal update to session model to store data from page `/accept-low-margin-fuel-card`

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -18,7 +18,7 @@ object CodeCoverageSettings extends AutoPlugin {
 
   override val projectSettings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 66.8,
+    ScoverageKeys.coverageMinimumStmtTotal := 66.9,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
