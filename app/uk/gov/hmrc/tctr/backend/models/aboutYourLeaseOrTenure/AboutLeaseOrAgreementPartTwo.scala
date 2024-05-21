8c075ec0383404b533bc5fc5e09db474518dc6a6
MJANOWICZ <michal.janowicz@digital.voa.gov.uk>
2024-05-21 12:21:58 +0100

    BST-92444 capitalSumDescription moved to AboutLeaseOrAgreementPartTwo

--- app/uk/gov/hmrc/tctr/backend/models/aboutYourLeaseOrTenure/AboutLeaseOrAgreementPartTwo.scala
+++ app/uk/gov/hmrc/tctr/backend/models/aboutYourLeaseOrTenure/AboutLeaseOrAgreementPartTwo.scala
@@ -33,6 +33,7 @@ case class AboutLeaseOrAgreementPartTwo(
   tenantAdditionsDisregardedDetails: Option[TenantAdditionsDisregardedDetails] = None,
   tenantsAdditionsDisregardedDetails: Option[TenantsAdditionsDisregardedDetails] = None,
   payACapitalSumDetails: Option[PayACapitalSumDetails] = None,
+  capitalSumDescription: Option[CapitalSumDescription] = None,
   paymentWhenLeaseIsGrantedDetails: Option[PaymentWhenLeaseIsGrantedDetails] = None,
   tenancyLeaseAgreementExpire: Option[TenancyLeaseAgreementExpire] = None,
   tenancyLeaseAgreementDetails: Option[TenancyLeaseAgreementDetails] = None,
