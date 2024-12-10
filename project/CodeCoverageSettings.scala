e5e30ee820726bcae425104e5a8d56a12b24a557
Yuriy Tumakha <tumakha@gmail.com>
2024-12-10 10:54:51 +0000

    BST-114057 Test coverage bumped form 65.54% to 70.49%

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -12,7 +12,7 @@ object CodeCoverageSettings extends AutoPlugin {
   override lazy val projectSettings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(","),
     ScoverageKeys.coverageExcludedFiles := excludedFiles.mkString(","),
-    ScoverageKeys.coverageMinimumStmtTotal := 65.5,
+    ScoverageKeys.coverageMinimumStmtTotal := 70.4,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
