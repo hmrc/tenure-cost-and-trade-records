2b1bff653b6d88880fc56da99d5ea39a29a1f98a
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2025-09-16 15:15:43 +0100

    BST-144115 Fix acceptance test failures and still createdAt to be updates

--- app/uk/gov/hmrc/tctr/backend/repository/CredentialsRepo.scala
+++ app/uk/gov/hmrc/tctr/backend/repository/CredentialsRepo.scala
@@ -124,8 +124,8 @@ class CredentialsMongoRepo @Inject() (mongo: MongoComponent, configuration: Conf
 
     def toBson(doc: JsObject): BsonDocument = {
       val withLastModified = doc + ("LastModified" -> JsString(Instant.now().toString))
-      val setData          = BsonDocument("$set" -> BsonDocument(Json.stringify(withLastModified)))
-      setData.append("$set", BsonDocument("createdAt" -> BsonDateTime(Instant.now().toEpochMilli)))
+      BsonDocument("$set" -> BsonDocument(Json.stringify(withLastModified))
+        .append("createdAt", BsonDateTime(Instant.now().toEpochMilli)))
     }
 
     val bulkOps: Seq[WriteModel[? <: FORCredentials]] = credentialsSeq.map { cred =>
