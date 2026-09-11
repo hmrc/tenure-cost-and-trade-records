be42d10a3f1fce4178fdc6b9a9bdc0f9090f18dd
Yuriy Tumakha <tumakha@gmail.com>
2026-09-11 11:31:37 +0100

    BST-167771 TCTR: Use com.ibm.icu:icu4j instead of deprecated uk.gov.hmrc:play-language

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 
 private object AppDependencies {
 
-  val bootstrapVersion          = "10.7.0"
-  val hmrcMongoVersion          = "2.12.0"
+  val bootstrapVersion          = "10.8.0"
+  val hmrcMongoVersion          = "2.13.0"
   val cryptoJsonVersion         = "8.4.0"
-  val playLanguageVersion       = "9.6.0"
   val internalAuthClientVersion = "3.1.0"
+  val ibmICUVersion             = "78.3"
 
   // Test dependencies
   val scalaTestPlusMockitoVersion = "3.2.19.0"
@@ -15,9 +15,9 @@ private object AppDependencies {
   private val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"           % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-language-play-30"        % playLanguageVersion,
     "uk.gov.hmrc"       %% "crypto-json-play-30"          % cryptoJsonVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % internalAuthClientVersion
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30" % internalAuthClientVersion,
+    "com.ibm.icu"        % "icu4j"                        % ibmICUVersion
   )
 
   private val commonTests = Seq(
